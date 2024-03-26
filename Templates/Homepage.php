650a96d8bdd87d81c667579d6866af0d588f2688
nzommmo <ericnzomo17@gmail.com>
2024-03-26 12:34:25 +0300

    news update

--- Templates/Homepage.php
+++ Templates/Homepage.php
@@ -12,6 +12,23 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+// Fetch titles and URLs from the database
+$sql = "SELECT title, article_url,image_url FROM Articles2";
+$result = mysqli_query($conn, $sql);
+
+// Array to store fetched articles
+$articles = array();
+
+if (mysqli_num_rows($result) > 0) {
+    // Fetch each row and add article details to the array
+    while($row = mysqli_fetch_assoc($result)) {
+        $articles[] = $row;
+    }
+} else {
+    echo "0 results found in the database";
+}
+
+
 
 // Function to fetch image from Unsplash API based on title
 function getImageFromUnsplash($title) {
@@ -54,7 +71,7 @@ function getImageFromUnsplash($title) {
 }
 
 // Fetch titles from the database
-$sql = "SELECT title FROM Articles";
+$sql = "SELECT title FROM Articles LIMIT 5";
 $result = mysqli_query($conn, $sql);
 
 // Array to store fetched titles
@@ -67,6 +84,40 @@ if (mysqli_num_rows($result) > 0) {
     }
 } else {
     echo "0 results found in the database";
+}
+// Function to fetch articles based on search query
+function searchArticles($conn, $query) {
+    $query = mysqli_real_escape_string($conn, $query);
+    $sql = "SELECT title, article_url, image_url FROM Articles2 WHERE title LIKE '%$query%'";
+    $result = mysqli_query($conn, $sql);
+    $articles = [];
+
+    if (mysqli_num_rows($result) > 0) {
+        // Fetch each row and add article details to the array
+        while($row = mysqli_fetch_assoc($result)) {
+            $articles[] = $row;
+        }
+    }
+
+    return $articles;
+}
+
+// Check if search query is present
+if(isset($_GET['q'])) {
+    // Fetch articles based on the search query
+    $articles = searchArticles($conn, $_GET['q']);
+} else {
+    // Fetch all articles if no search query is present
+    $sql = "SELECT title, article_url, image_url FROM Articles2";
+    $result = mysqli_query($conn, $sql);
+    $articles = [];
+
+    if (mysqli_num_rows($result) > 0) {
+        // Fetch each row and add article details to the array
+        while($row = mysqli_fetch_assoc($result)) {
+            $articles[] = $row;
+        }
+    }
 }
 
 // Close database connection
@@ -85,7 +136,9 @@ mysqli_close($conn);
 
     <title>MamboLeoNews</title>
 </head>
-<body><nav class="navbar navbar-expand-lg navbar-light bg-light">
+<body>
+    
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">MamboLeoNews</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -96,6 +149,11 @@ mysqli_close($conn);
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../Templates/login.php">Login</a>
+        </li>
+
+        <!--
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Local News
@@ -152,45 +210,46 @@ mysqli_close($conn);
       
       </ul>
 
-    
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    -->
+      <form class="d-flex" method="GET">
+        <input class="form-control me-2" type="search"  name="q"    placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
 </nav>
+ <!-- Carousel -->
+ <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <?php foreach ($carousel_titles as $index => $title) : ?>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="<?php echo $index; ?>" <?php echo ($index == 0) ? 'class="active"' : ''; ?> aria-current="true" aria-label="Slide <?php echo $index + 1; ?>"></button>
+            <?php endforeach; ?>
+        </div>
+        <div class="carousel-inner" id="caro">
+            <?php foreach ($carousel_titles as $index => $title) : ?>
+                <?php
+                // Fetch image from Unsplash API
+                $response = getImageFromUnsplash($title);
 
-<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-        <?php foreach ($titles as $index => $title): ?>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="<?php echo $index; ?>" <?php echo ($index == 0) ? 'class="active"' : ''; ?> aria-current="true" aria-label="Slide <?php echo $index + 1; ?>"></button>
-        <?php endforeach; ?>
-    </div>
-    <div class="carousel-inner" id="caro">
-        <?php foreach ($titles as $index => $title): ?>
-            <?php
-            // Fetch image from Unsplash API
-            $response = getImageFromUnsplash($title);
-
-            // Check if image found for the title
-            if(isset($response['results'][0]['urls']['regular'])) {
-                $imageUrl = $response['results'][0]['urls']['regular'];
-                $activeClass = ($index == 0) ? 'active' : ''; // Add 'active' class to first image
+                // Check if image found for the title
+                if (isset($response['results'][0]['urls']['regular'])) {
+                    $imageUrl = $response['results'][0]['urls']['regular'];
+                    $activeClass = ($index == 0) ? 'active' : ''; // Add 'active' class to first image
                 ?>
-                <div class="carousel-item <?php echo $activeClass; ?>">
-                    <img src="<?php echo $imageUrl; ?>" class="d-block w-100" alt="Image for <?php echo $title; ?>">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5><?php echo $title; ?></h5>
-                        <p>Some representative placeholder content for the <?php echo $title; ?> slide.</p>
+                    <div class="carousel-item <?php echo $activeClass; ?>">
+                        <img src="<?php echo $imageUrl; ?>" class="d-block w-100" alt="Image for <?php echo $title; ?>">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5><?php echo $title; ?></h5>
+                            <p>Some representative placeholder content for the <?php echo $title; ?> slide.</p>
+                        </div>
                     </div>
-                </div>
-            <?php } else {
-                echo 'Image not found for ' . $title;
-            }
+            <?php
+                } else {
+                    echo 'Image not found for ' . $title;
+                }
+            endforeach;
             ?>
-        <?php endforeach; ?>
-    </div>
+        </div>    </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
@@ -201,94 +260,26 @@ mysqli_close($conn);
     </button>
 </div>
 
-
 <div class="container mt-5">
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-        <!-- News cards -->
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">News Title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
+        <!-- Display news cards -->
+        <?php foreach ($articles as $article): ?>
+            <div class="col">
+                <div class="card">
+                    <!-- Display image from the database -->
+                    <img src="<?php echo $article['image_url']; ?>" class="card-img-top" alt="Article Image">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $article['title']; ?></h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="<?php echo $article['article_url']; ?>" class="btn btn-primary">Read More</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">News Title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">News Title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">News Title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">News Title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">News Title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">News Title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">News Title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Read More</a>
-                </div>
-            </div>
-        </div>
-        <!-- Add more similar card divs for additional news -->
+        <?php endforeach; ?>
 
     </div>
 </div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
--- Templates/Scrapper.php
+++ Templates/Scrapper.php
@@ -1,65 +1,101 @@
 <?php
-$url = 'https://www.standardmedia.co.ke/'; // Replace with the URL of the website you want to scrape
+function scrapeAndUploadArticles() {
+    $url = 'https://www.standardmedia.co.ke/'; // Replace with the URL of the website you want to scrape
 
-// Fetch the content from the URL
-$html = file_get_contents($url);
+    // Fetch the content from the URL
+    $html = file_get_contents($url);
 
-if ($html === false) {
-    // Handle error if unable to fetch content
-    echo "Error fetching URL";
-} else {
-    // Parse the HTML content to extract relevant data
-    $doc = new DOMDocument();
-    libxml_use_internal_errors(true); // Disable libxml errors
-    $doc->loadHTML($html);
-    libxml_clear_errors();
+    if ($html === false) {
+        // Handle error if unable to fetch content
+        echo "Error fetching URL";
+    } else {
+        // Parse the HTML content to extract relevant data
+        $doc = new DOMDocument();
+        libxml_use_internal_errors(true); // Disable libxml errors
+        $doc->loadHTML($html);
+        libxml_clear_errors();
 
-    // Initialize an array to store articles
-    $articles = [];
+        // Initialize an array to store articles
+        $articles = [];
 
-    // Find all anchor tags (links) with a specific class (you may need to inspect the HTML of the website to find appropriate selectors)
-    $links = $doc->getElementsByTagName('a');
-    foreach ($links as $link) {
-        $href = $link->getAttribute('href');
-        $text = trim($link->nodeValue);
+        // Find all anchor tags (links) with a specific class (you may need to inspect the HTML of the website to find appropriate selectors)
+        $links = $doc->getElementsByTagName('a');
+        foreach ($links as $link) {
+            $href = $link->getAttribute('href');
+            $text = trim($link->nodeValue);
 
-        // Check if the link leads to an article
-        if (strpos($href, '/article/') !== false && !empty($text)) {
-            // Store the article link and title in the articles array
-            $articles[] = [
-                'title' => $text,
-                'url' => $href
-            ];
+            // Check if the link leads to an article
+            if (strpos($href, '/article/') !== false && !empty($text)) {
+                // Store the article link and title in the articles array
+                $articles[] = [
+                    'title' => $text,
+                    'url' => $href,
+                    'image' => ''
+                ];
+            }
         }
-    }
 
-    // Insert the extracted data into the database
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "News";
+        // Find all image tags with a specific class (you may need to inspect the HTML of the website to find appropriate selectors)
+        $images = $doc->getElementsByTagName('img');
+        $imageIndex = 0;
+        foreach ($images as $image) {
+            $src = $image->getAttribute('src');
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-
-    // Insert each article into the database
-    foreach ($articles as $article) {
-        $title = mysqli_real_escape_string($conn, $article['title']);
-        $url = mysqli_real_escape_string($conn, $article['article_url']);
-        
-        // SQL query to insert data into the database
-        $sql = "INSERT INTO Articles (title, article_url) VALUES ('$title', '$url')";
-
-        if (mysqli_query($conn, $sql)) {
-            echo "Article inserted successfully: $title <br>";
-        } else {
-            echo "Error inserting article: $title <br>";
+            // Store the image URL in the corresponding article
+            if (!empty($src) && $imageIndex < count($articles)) {
+                $articles[$imageIndex]['image'] = $src;
+                $imageIndex++;
+            }
         }
-    }
 
-    mysqli_close($conn);
+        // Connect to the database
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "News";
+
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        // Check if articles already exist in the database
+        $existingArticles = [];
+        $result = mysqli_query($conn, "SELECT title, article_url FROM Articles2");
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $existingArticles[$row['title']] = $row['article_url'];
+            }
+            mysqli_free_result($result);
+        }
+
+        // Insert new articles into the database
+        foreach ($articles as $article) {
+            $title = mysqli_real_escape_string($conn, $article['title']);
+            $url = mysqli_real_escape_string($conn, $article['url']);
+            $image = mysqli_real_escape_string($conn, $article['image']);
+            $summary = mysqli_real_escape_string($conn, $article['summary']);
+
+
+            // Check if the article is new
+            if (!isset($existingArticles[$title])) {
+                // SQL query to insert data into the database
+                $sql = "INSERT INTO Articles2 (title, summary,article_url, image_url) VALUES ('$title', '$summary','$url', '$image')";
+
+                if (mysqli_query($conn, $sql)) {
+                    echo "New article inserted successfully: $title <br>";
+                } else {
+                    echo "Error inserting new article: $title <br>";
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+            }
+        }
+
+        mysqli_close($conn);
+    }
 }
+
+// Run the scraping and uploading function
+scrapeAndUploadArticles();
 ?>
